e8bef4c2bfc5492b3991b03633a842725d2e2c7d
Alejandro <alemancar0511@gmail.com>
2024-06-20 12:01:24 -0500

    cambios

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =============== TYPING ANIMATION =============== */
+var typed = new Typed(".typing", {
+    strings:["","Web Desing","Web Developer","Graphic Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})

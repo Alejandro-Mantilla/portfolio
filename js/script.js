679794102a1a5f55075b6dea93d16ded959b5d08
Alejandro <alemancar0511@gmail.com>
2024-06-20 19:52:19 -0500

    Arreglos en el JS

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ var typed = new Typed(".typing", {
     BackSpeed: 60,
     loop: true
 })
+
+/* =============== ASIDE =============== */
+const nav = document.querySelector(".nav"),
+      navList = document.querySelectorAll("li"),
+      totalNavList = navList.length;
+      for(let i=0; i<totalNavList; i++)
+      {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            console.log(this)
+        })
+      }

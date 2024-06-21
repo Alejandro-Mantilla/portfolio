042bd01aeea45ada2ffefa8aebd5f74640d5fc21
Alejandro <alemancar0511@gmail.com>
2024-06-21 12:48:11 -0500

    Add main.js file with code for EnailJS functionality

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* =============== EMAIL JS =============== */
+
+function() {
+    emiljs.init('Alejandro Mantilla');
+};
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let userName = event.target.user_name.value;
+    let userEmail = event.target.user_email.value;
+    let userMessage = event.target.user_message.value;
+
+    let templateParams = {
+        user_name: userName,
+        user_email: userEmail,
+        user_message: userMessage
+    };
+
+    emiljs.send('dardro0511', 'dardro11', templateParams)
+    .then(function(response) {
+        alert('Your message  was sent successfully: ' + response.status);
+    }, function(error) {
+        alert('Error sending message: ' + error.text);
+    });
+})
